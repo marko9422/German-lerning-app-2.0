9b05d8c3950c255a3ae7d85cd3c702048971c566
Marko Sarcevic <sarcevic.m@outlook.com>
2023-11-30 09:25:03 +0100

    Added lihe heigth

--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -2,14 +2,16 @@ import React, { useRef } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 
 export default function App() {
+
     const editorRef = useRef(null);
     const log = () => {
         if (editorRef.current) {
             console.log(editorRef.current.getContent());
         }
     };
+    
     return (
-        <>
+        <div className={styles.editor}>
             <Editor
                 apiKey='ov19oabyq813xxlmgyhlhoqpx18jqzdrd5eq7pxyg0wxsvkt'
                 onInit={(evt, editor) => editorRef.current = editor}
@@ -17,17 +19,24 @@ export default function App() {
                 init={{
                     height: 500,
                     menubar: false,
+                    line_height_formats: '0.5 1 1.2 1.4 1.6 2 5',
                     plugins: [
-                        'export','advlist','Preview' , 'lists','pagebreak',
+                        'export','advlist','preview' , 'lists','pagebreak',
                         'anchor','searchreplace', 'visualblocks', 'code', 'fullscreen','table'
                     ],
-                    toolbar: 'Preview pagebreak undo redo' +
-                        'bold italic forecolor backcolor fontsize|  alignleft aligncenter ' +
-                        'alignright alignjustify table export',
-                    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+                    toolbar: 'preview pagebreak undo redo' +
+                        'italic bold  forecolor backcolor fontsize|  alignleft aligncenter ' +
+                        'alignright alignjustify table export lineheight',
+                    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px; line-heigth:20px; }'
                 }}
             />
             <button onClick={log}>Log editor content</button>
-        </>
+        </div>
     );
 }
+
+const styles = {
+    editor: {
+        width: '800px', 
+    }
+};
