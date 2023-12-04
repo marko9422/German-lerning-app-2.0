74205aca2a0dfec56d3a1352e432adee7bff9524
Marko Sarcevic <sarcevic.m@outlook.com>
2023-12-04 11:07:39 +0100

    Created edit button on update updated firestore.

--- src/components/ListGrammar.js
+++ src/components/ListGrammar.js
@@ -1,16 +1,31 @@
-import { React } from 'react';
+import { React,useState } from 'react';
 
 import useFetchGrammar from '../hooks/useFetchGrammar';
+import EditGrammar from './EditGrammar';
 
 export default function ListGrammar() {
 
     const { loading, grammar } = useFetchGrammar()
+    const [edit,setEdit] = useState('')
+
+    const editGrammar = (id) => {
+        setEdit(id)
+    }
 
     return (
         <div className='container'>
-            <div>{loading ? 'loading...' : 'grammar'}
-                {grammar.map(({ id, answer }) => (
+            <div>{loading ? 'loading...' 
+                : 
+                grammar.map(({ id, answer }) => (
+                    
+                    edit == id ? <div>
+                                    <p>edit:   {edit}id:   {id}</p> 
+                                    <EditGrammar initialValue={answer} id={id}></EditGrammar>
+                                </div>
+                    :
+                    
                     <div style={styles.grammarContainer} className='testStyle' key={id}>
+                        <p>edit:   {edit}id:   {id}</p> 
                         <div
                             style={{
                                 fontFamily: 'Helvetica, Arial, sans-serif',
@@ -18,12 +33,13 @@ export default function ListGrammar() {
                             }}
                             dangerouslySetInnerHTML={{ __html: answer }}
                         />
+                        <button onClick={() => editGrammar(id)}>EDIT</button>
                     </div>
-                ))}
+                ))
+                }
+                
             </div>
 
-
-            {/* {console.log(data.asnwer)} */}
         </div>
     )
 
