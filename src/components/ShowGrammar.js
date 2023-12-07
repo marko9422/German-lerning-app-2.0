6335ee1d6f48a14266f2497fe74bcd9be738a583
Marko Sarcevic <sarcevic.m@outlook.com>
2023-12-07 20:22:59 +0100

    deleted created ShowGrammar.js

--- src/components/ShowGrammar.js
+++ src/components/ShowGrammar.js
@@ -2,32 +2,8 @@ import React from 'react'
 
 export default function ShowGrammar(props) {
 
-  const editGrammar = (id) => {
-    setEdit(id)
-    setEditorToeditCurrentGrammar(true)
-    setCurrentlyEditing(randomlengthOfGrammar)
-}
 
   return (
-      <div key={props.id}>
-        <div style={styles.grammarContainer} className='testStyle'>
-          <div
-            style={{
-              fontFamily: 'Helvetica, Arial, sans-serif',
-              fontSize: '12pt'
-            }}
-            dangerouslySetInnerHTML={{ __html: answer }}
-          />
-        </div>
-        <button onClick={() => editGrammar(id)}>EDIT</button>
-        <CorrectGrammarButton id={id} score={score} />
-      </div>
-
+    <div>hi</div>
   )
 }
-const styles = {
-  grammarContainer: {
-      border: '2px solid black',
-      margin: '10px',
-  }
-};
