47d7de85ee7b0904a02c9c7718d7f3207b424735
Marko Sarcevic <sarcevic.m@outlook.com>
2023-12-08 21:47:57 +0100

    Edit word is working.

---
+++ src/components/EditWords.js
@@ -0,0 +1,81 @@
+import { useRef, useState, useEffect } from 'react';
+import { db } from '../firebase/config'
+import { doc, collection, updateDoc } from 'firebase/firestore';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+export default function AddNewWord(props) {
+
+    const [inputs, setInputs] = useState({
+        englishShortText: props.english || '',
+        germanShortText: props.german || '',
+    });
+
+    const userCollectionRef = collection(db, 'words')
+    const editorRef = useRef(null);
+
+    const editWordsInsideDatabase = async () => {
+        const textareaDoc = doc(db, 'words', props.id)
+        const updateTextarea = {
+            english: inputs.englishShortText,
+            german: inputs.germanShortText,
+        }
+        await updateDoc(textareaDoc, updateTextarea);
+    }
+
+    // setLoadingAfterPost(false)
+    // props?.callback(false)
+    // setLoadingAfterPost(true)
+
+
+
+
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setInputs(values => ({ ...values, [name]: value }))
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // FILL THE TEXT ALLERT IF IS IMPUT EMPTY.
+        if (!inputs.englishShortText || !inputs.germanShortText) {
+            alert('Please fill in the text.');
+            return;
+        } else {
+            editWordsInsideDatabase()
+            setInputs({})
+        }
+
+    }
+
+    return (
+        <div className='container'>
+            <Form onSubmit={handleSubmit} >
+                <Form.Group className="mb-3"  >
+                    <Form.Control
+                        name='englishShortText'
+                        value={inputs.englishShortText || props.english}
+                        placeholder='ENGLISH'
+                        onChange={handleChange} type="text"
+                        autoComplete="off" />
+                </Form.Group>
+
+                <Form.Group className="mb-3"  >
+                    <Form.Control
+                        name='germanShortText'
+                        value={inputs.germanShortText || props.german}
+                        placeholder='GERMAN'
+                        onChange={handleChange}
+                        type="text"
+                        autoComplete="off" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">Submit</Button>
+
+            </Form>
+
+        </div>
+    )
+}
--- src/components/ListWords.js
+++ src/components/ListWords.js
@@ -1,12 +1,19 @@
 import { React, useState, useEffect } from 'react';
 import useFetchWords from '../hooks/useFetchWords'
 import ButtonsForWords from './ButtonsForWords';
+import EditWords from './EditWords'
 
 
 export default function ListWords() {
 
     const [loading, words] = useFetchWords()
     const [chooseListedLanguage, setChooseListedLanguage] = useState('english')
+    const [editingThisWords, setEditingThisWords] = useState(false)
+
+    const editWords = (id) => {
+        setEditingThisWords(true)
+
+    }
 
     // Set listed language of the first words.
     const setEnglishLanguage = () =>{
@@ -28,18 +35,24 @@ export default function ListWords() {
                 // Loaded words from firebase...
                 words.map(({ id, english, german, englishScore, germanScore, visible }, index) => (
                     <div key={id}>
-                    {chooseListedLanguage == 'german' ?
-                        (<>
-                        <p>{english}</p>
-                        <p>{german}</p>
-                        <ButtonsForWords id={id} chooseListedLanguage={chooseListedLanguage} englishScore={englishScore} germanScore={germanScore}></ButtonsForWords>
-                        </>) : (
-                        <>
-                        <p>{german}</p>
-                        <p>{english}</p>
-                        <ButtonsForWords id={id} chooseListedLanguage={chooseListedLanguage} englishScore={englishScore} germanScore={germanScore}></ButtonsForWords>
-                        </>)
+                    {editingThisWords == false ?
+                        chooseListedLanguage == 'german' ?
+                            (<>
+                            <p>{english}</p>
+                            <p>{german}</p>
+                            <ButtonsForWords id={id} chooseListedLanguage={chooseListedLanguage} englishScore={englishScore} germanScore={germanScore}></ButtonsForWords>
+                            <button onClick={() => editWords(id)}>edit</button>
+                            </>) : (
+                            <>
+                            <p>{german}</p>
+                            <p>{english}</p>
+                            <ButtonsForWords id={id} chooseListedLanguage={chooseListedLanguage} englishScore={englishScore} germanScore={germanScore}></ButtonsForWords>
+                            <button onClick={() => editWords(id)}>edit</button>
+                            </>)
+                        
+                    : <EditWords id={id} english={english} german={german}></EditWords>
                     }
+
                     </div>
                 ))
             )}
